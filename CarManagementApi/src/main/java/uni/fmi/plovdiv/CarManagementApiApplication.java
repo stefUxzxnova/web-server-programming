b1bc20438a115d1a3ae92824176a2410162db8ca
Stefani Uzunova <94936224+stefUxzxnova@users.noreply.github.com>
2024-12-29 01:05:08 +0200

    Initial commit

---
+++ CarManagementApi/src/main/java/uni/fmi/plovdiv/CarManagementApiApplication.java
@@ -0,0 +1,13 @@
+package uni.fmi.plovdiv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarManagementApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarManagementApiApplication.class, args);
+	}
+
+}
